1ff81607ecb9040ce798cb2266068813c402a4a7
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-11-23 17:29:03 +0530

    new

---
+++ Assets/Hacker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        showMainMenu();
+    }
+
+    // main menu function
+    void showMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local libraries");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("\nEnter your selection:");
+    }
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            showMainMenu();
+        }
+        else if (input == "clear")
+        {
+            Terminal.ClearScreen();
+        }
+    }
+
+}
